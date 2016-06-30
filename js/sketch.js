c904024b73bcadf16f48b4451727e2e227545637
JulioMontas <julio86@gmail.com>
2016-06-30 14:47:22 -0400

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,56 @@
+// Code by Daniel Shiffman: https://youtu.be/17WoOqgXsRM
+
+var stars = []
+var speed;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (var i = 0; i < 800; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  speed = map(800, 0, width, 0, 50);
+  background(255);
+  translate(width / 2, height / 2);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
+
+function Star() {
+  this.x = random(-width, width);
+  this.y = random(-height, height);
+  this.z = random(width);
+  this.pz = this.z;
+
+  this.update = function() {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  }
+
+  this.show = function() {
+
+    var sx = map(this.x / this.z, 0, 1, 0, width);
+    var sy = map(this.y / this.z, 0, 1, 0, height);
+
+    var r = map(this.z, 0, width, 16, 0);
+
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(19,105,187);
+    strokeWeight(2);
+    line(px, py, sx, sy);
+
+  }
+}
